f6453646742fe1e2a6c555c744565faba510bc04
Matthew Smith <usryokousha@gmail.com>
2023-07-18 23:38:55 +0900

    added RestrictedRandomResizedCrop

--- src/transforms.py
+++ src/transforms.py
@@ -9,10 +9,18 @@ from logging import getLogger
 
 from PIL import ImageFilter
 
+import math
+import warnings
+
 import torch
+from torch import Tensor
+
 import PIL.Image as Image
 import torchvision.transforms as transforms
 from torchvision.transforms import functional as F
+from torchvision.transforms.transforms import _setup_size
+
+from typing import Optional, Union, List, Tuple, Sequence
 
 _GLOBAL_SEED = 0
 logger = getLogger()
@@ -178,5 +186,83 @@ class Downsample(torch.nn.Module):
     def forward(self, img):
         hr, wr = self.get_params(img, self.scale_factor)
         return F.resize(img, (hr, wr), Image.BICUBIC)
+    
+    
+class RestrictedRandomResizedCrop(torch.nn.Module):
+    def __init__(
+        self,
+        size,
+        restriction,
+        scale=(0.08, 1.0),
+        ratio=(3.0 / 4.0, 4.0 / 3.0),
+        interpolation=transforms.InterpolationMode.BILINEAR,
+        antialias: Optional[Union[str, bool]] = "warn",
+    ):
+        super().__init__()
+        self.size = _setup_size(size, error_msg="Please provide only two dimensions (h, w) for size.")
+        self.restriction = restriction
+
+        if not isinstance(scale, Sequence):
+            raise TypeError("Scale should be a sequence")
+        if not isinstance(ratio, Sequence):
+            raise TypeError("Ratio should be a sequence")
+        if (scale[0] > scale[1]) or (ratio[0] > ratio[1]):
+            warnings.warn("Scale and ratio should be of kind (min, max)")
+
+        if isinstance(interpolation, int):
+            interpolation = F._interpolation_modes_from_int(interpolation)
+
+        self.interpolation = interpolation
+        self.antialias = antialias
+        self.scale = scale
+        self.ratio = ratio
+
+    @staticmethod
+    def get_params(img: Tensor, scale: List[float], ratio: List[float], restriction: float) -> Tuple[int, int, int, int]:
+        _, height, width = F.get_dimensions(img)
+        area = height * width
+
+        log_ratio = torch.log(torch.tensor(ratio))
+        for _ in range(10):
+            target_area = area * torch.empty(1).uniform_(scale[0], scale[1]).item()
+            aspect_ratio = torch.exp(torch.empty(1).uniform_(log_ratio[0], log_ratio[1])).item()
+
+            w = int(round(math.sqrt(target_area * aspect_ratio)))
+            h = int(round(math.sqrt(target_area / aspect_ratio)))
+
+            if 0 < w <= width and 0 < h <= height:
+                i = max(0, min(height - h, int((height - h) / 2 + torch.randn(1).item() * restriction * (height - h) / 2)))
+                j = max(0, min(width - w, int((width - w) / 2 + torch.randn(1).item() * restriction * (width - w) / 2)))
+                return i, j, h, w
+
+        # Fallback to central crop
+        in_ratio = float(width) / float(height)
+        if in_ratio < min(ratio):
+            w = width
+            h = int(round(w / min(ratio)))
+        elif in_ratio > max(ratio):
+            h = height
+            w = int(round(h * max(ratio)))
+        else:  # whole image
+            w = width
+            h = height
+        i = (height - h) // 2
+        j = (width - w) // 2
+        return i, j, h, w
+
+    def forward(self, img):
+        i, j, h, w = self.get_params(img, self.scale, self.ratio, self.restriction)
+        return F.resized_crop(img, i, j, h, w, self.size, self.interpolation, antialias=self.antialias)
+
+    def __repr__(self) -> str:
+        interpolate_str = self.interpolation.value
+        format_string = self.__class__.__name__ + f"(size={self.size}"
+        format_string += f", restriction={self.restriction}"
+        format_string += f", scale={tuple(round(s, 4) for s in self.scale)}"
+        format_string += f", ratio={tuple(round(r, 4) for r in self.ratio)}"
+        format_string += f", interpolation={interpolate_str}"
+        format_string += f", antialias={self.antialias})"
+        return format_string
+
 
     
